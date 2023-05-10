bfc0e64bee2351ba79cf53cc237d53bd43ed5fa1
stefanolonghena <stefano.longhena@gmail.com>
2023-05-10 09:16:15 +0200

    started home page

---
+++ lib/model/category.dart
@@ -0,0 +1,13 @@
+
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+class Category {
+  Category({required this.name});
+
+  final id = uuid.v4();
+  final String name;
+
+
+}
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:pecunia/screens/home.dart';
 
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
@@ -26,6 +27,16 @@ class MyApp extends StatelessWidget {
       animation: settingsController,
       builder: (BuildContext context, Widget? child) {
         return MaterialApp(
+          
+          
+          home: Home(),
+          
+          
+          
+          
+          
+          
+          
           // Providing a restorationScopeId allows the Navigator built by the
           // MaterialApp to restore the navigation stack when a user leaves and
           // returns to the app after it has been killed while running in the
