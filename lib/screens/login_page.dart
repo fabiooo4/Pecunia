423134aa1efbe4c0da7e1174fe17bafc9f1056e5
andrebellu <bellu.andrea@outlook.it>
2023-05-16 13:34:22 +0200

    added social buttons for login

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -31,113 +31,151 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height / 2.5,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 7, 46, 8),
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.elliptical(110, 110),
-                          bottomRight: Radius.elliptical(100, 100))),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Transform(
-                        transform: Matrix4.translationValues(0, 50, 0),
-                        child: const CircleAvatar(
-                          radius: 55,
-                          backgroundColor: Colors.white,
-                          child: Icon(
-                            Icons.attach_money_rounded,
-                            size: 50,
-                            color: Colors.black,
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Center(
+              child: Column(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height / 2.5,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                        color: Color.fromARGB(255, 7, 46, 8),
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.elliptical(110, 110),
+                            bottomRight: Radius.elliptical(100, 100))),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Transform(
+                          transform: Matrix4.translationValues(0, 50, 0),
+                          child: const CircleAvatar(
+                            radius: 55,
+                            backgroundColor: Colors.white,
+                            child: Icon(
+                              Icons.attach_money_rounded,
+                              size: 50,
+                              color: Colors.black,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  const Text(
+                    'Pecunia',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.lightGreen),
+                  ),
+                  const Text(
+                    'Handle them coins, dawg!',
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black),
+                  ),
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextField(
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              labelText: 'Email',
+                              labelStyle: TextStyle(color: Colors.black),
+                              suffixIcon: Padding(
+                                padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                child: IconButton(
+                                  onPressed: null,
+                                  icon: Icon(Icons.email),
+                                ),
+                              )),
+                          controller: _emailController,
+                        ),
+                        const SizedBox(height: 20),
+                        TextField(
+                          obscureText: _isObscure,
+                          decoration: InputDecoration(
+                              border: const OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              labelText: 'Password',
+                              labelStyle: const TextStyle(color: Colors.black),
+                              suffixIcon: Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                child: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _isObscure = !_isObscure;
+                                    });
+                                  },
+                                  icon: Icon(_isObscure
+                                      ? Icons.visibility
+                                      : Icons.visibility_off),
+                                ),
+                              )),
+                          controller: _passwordController,
+                        ),
+                        const SizedBox(height: 20),
+                        FilledButton(
+                          style: ButtonStyle(
+                            minimumSize: MaterialStateProperty.all(
+                                const Size(double.infinity, 50)),
+                          ),
+                          onPressed: () {
+                            var email = _emailController.text;
+                            var password = _passwordController.text;
+
+                            print('Email: $email');
+                            print('Password: $password');
+                          },
+                          child: const Text('Login'),
+                        ),
+                        const SizedBox(height: 10),
+                        /*  const InkWell(
+                          child: Text(
+                            'or Signup',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.normal),
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 30),
-                const Text(
-                  'Pecunia',
-                  style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.lightGreen),
-                ),
-                const Text(
-                  'Handle them coins, dawg!',
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black),
-                ),
-                const SizedBox(height: 30),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextField(
-                        decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                            labelText: 'Email',
-                            labelStyle: TextStyle(color: Colors.black),
-                            suffixIcon: Padding(
-                              padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-                              child: IconButton(
+                       */
+                        Row(children: const [
+                          Expanded(child: Divider()),
+                          Text("  or  "),
+                          Expanded(child: Divider()),
+                        ]),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            IconButton(
+                              onPressed: null,
+                              icon: Icon(Icons.apple),
+                              iconSize: 40,
+                            ),
+                            SizedBox(width: 10),
+                            // do the same for google
+                            IconButton(
                                 onPressed: null,
-                                icon: Icon(Icons.email),
-                              ),
-                            )),
-                        controller: _emailController,
-                      ),
-                      const SizedBox(height: 20),
-                      TextField(
-                        obscureText: _isObscure,
-                        decoration: InputDecoration(
-                            border: const OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                            labelText: 'Password',
-                            labelStyle: const TextStyle(color: Colors.black),
-                            suffixIcon: Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
-                              child: IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    _isObscure = !_isObscure;
-                                  });
-                                },
-                                icon: Icon(_isObscure
-                                    ? Icons.visibility
-                                    : Icons.visibility_off),
-                              ),
-                            )),
-                        controller: _passwordController,
-                      ),
-                      const SizedBox(height: 20),
-                      FilledButton(
-                        onPressed: () {
-                          var email = _emailController.text;
-                          var password = _passwordController.text;
-
-                          print('Email: $email');
-                          print('Password: $password');
-                        },
-                        child: Text('Login'),
-                      ),
-                    ],
+                                icon: Icon(Icons.android),
+                                iconSize: 40),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ));
