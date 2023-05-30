84d6d24c2b440b2a28705216f27b455c07368157
fabiooo4 <fabioirimie@gmail.com>
2023-05-30 19:53:08 +0200

    edited style to add modal

--- lib/widgets/transaction_modal.dart
+++ lib/widgets/transaction_modal.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pecunia/src/utils/capitalize.dart';
 
-enum TransactionType { income, expense }
+enum TransactionType { expense, income }
 
 class TransactionModal extends ConsumerStatefulWidget {
   const TransactionModal({super.key, required this.modalContext});
@@ -26,18 +26,27 @@ class _TransactionModalState extends ConsumerState<TransactionModal> {
 
   @override
   Widget build(BuildContext context) {
+    Color addColor = Colors.red;
+    if (_expenseType == 'expense') {
+      addColor = Colors.red;
+    } else {
+      addColor = Colors.blue;
+    }
     return Center(
       child: Column(children: [
         const SizedBox(height: 20),
         const Text('Add Transaction',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700)),
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.black)),
         DefaultTabController(
           length: TransactionType.values.length,
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               TabBar(
-                indicatorColor: Colors.lightGreen,
+                indicatorColor: addColor,
                 onTap: (index) {
                   setState(() {
                     _expenseType = TransactionType.values[index]
@@ -53,24 +62,174 @@ class _TransactionModalState extends ConsumerState<TransactionModal> {
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.3,
-                width: MediaQuery.of(context).size.width * 0.8,
+                width: MediaQuery.of(context).size.width,
                 child: TabBarView(
                   children: TransactionType.values
                       .map((type) => Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 10),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                TextField(
-                                  controller: _descriptionController,
-                                  decoration: const InputDecoration(
-                                      border: UnderlineInputBorder(),
-                                      labelText: 'Description'),
+                                const Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 18),
+                                    Text(
+                                      'Date',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black54,
+                                      ),
+                                    ),
+                                    SizedBox(height: 21),
+                                    Text(
+                                      'Account',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black54,
+                                      ),
+                                    ),
+                                    SizedBox(height: 21),
+                                    Text(
+                                      'Category',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black54,
+                                      ),
+                                    ),
+                                    SizedBox(height: 21),
+                                    Text(
+                                      'Amount',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black54,
+                                      ),
+                                    ),
+                                    SizedBox(height: 21),
+                                    Text(
+                                      'Title',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black54,
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                                const TextField(
-                                  decoration: InputDecoration(
-                                      border: UnderlineInputBorder(),
-                                      labelText: 'Amount'),
+                                const SizedBox(width: 20),
+                                Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.6,
+                                      height: 40,
+                                      child: TextField(
+                                        controller: _descriptionController,
+                                        decoration: InputDecoration(
+                                          enabledBorder:
+                                              const UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Colors.black12),
+                                          ),
+                                          focusedBorder: UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: addColor,
+                                              width: 2,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.6,
+                                      height: 40,
+                                      child: TextField(
+                                        controller: _descriptionController,
+                                        decoration: InputDecoration(
+                                          enabledBorder:
+                                              const UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Colors.black12),
+                                          ),
+                                          focusedBorder: UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: addColor,
+                                              width: 2,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.6,
+                                      height: 40,
+                                      child: TextField(
+                                        controller: _descriptionController,
+                                        decoration: InputDecoration(
+                                          enabledBorder:
+                                              const UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Colors.black12),
+                                          ),
+                                          focusedBorder: UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: addColor,
+                                              width: 2,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.6,
+                                      height: 40,
+                                      child: TextField(
+                                        controller: _descriptionController,
+                                        decoration: InputDecoration(
+                                          enabledBorder:
+                                              const UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Colors.black12),
+                                          ),
+                                          focusedBorder: UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: addColor,
+                                              width: 2,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.6,
+                                      height: 40,
+                                      child: TextField(
+                                        controller: _descriptionController,
+                                        decoration: InputDecoration(
+                                          enabledBorder:
+                                              const UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Colors.black12),
+                                          ),
+                                          focusedBorder: UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: addColor,
+                                              width: 2,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
